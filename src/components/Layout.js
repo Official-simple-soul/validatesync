73a4244e147f0b901d24b20b84d5119570acec63
Simple <mac@MACs-MacBook-Pro.local>
2023-08-03 15:48:50 +0100

    in progress

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,17 +5,17 @@ function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>ContractFixs</title>
+        <title>ValidateSync</title>
         <meta
           name='description'
           content='Blockchain Rectification - We are here to help you resolve your crypto related issues'
         />
-        <meta property='og:title' content='ContractFixs' />
+        <meta property='og:title' content='Validate Sync' />
         <meta
           property='og:image'
           content='https://img.favpng.com/12/9/21/blockchain-vector-graphics-computer-icons-illustration-bitcoin-png-favpng-7TJzKAfyQeJFBwa2EduZhFjD1.jpg'
         />
-        <meta property='og:url' content='https://contractfixs.vercel.app/' />
+        <meta property='og:url' content='https://validatesync.vercel.app/' />
         <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
       </Head>
       <main className='bg-[#001132]'>{children}</main>
